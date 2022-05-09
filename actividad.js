8f4235fe47cb8b087f2ac0141168e821eb6e87b5
Tomas Pfingsthorn <tpfingsthorn@miuandes.cl>
2022-05-09 17:56:53 -0400

    primer commit

--- actividad.js
+++ actividad.js
@@ -3,4 +3,15 @@ function say(name) {
     console.log('Hola '+ name);
 }
 
-say('tomas')
+function fibonacci(n) {
+    if (n == 0) {
+        return 1;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+a = fibonacci(5);
+console.log(a);
